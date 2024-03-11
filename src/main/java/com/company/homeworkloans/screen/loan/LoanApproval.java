aa0b336a155d0e76936938807b8bfcfac0e8aa19
SobolevEA <SobolevEA@dgtserv.ru>
2024-03-11 17:10:18 +0300

    2.1 (*) (опциональное) Отображение предыдущих займов клиента

--- src/main/java/com/company/homeworkloans/screen/loan/LoanApproval.java
+++ src/main/java/com/company/homeworkloans/screen/loan/LoanApproval.java
@@ -3,6 +3,9 @@ package com.company.homeworkloans.screen.loan;
 import io.jmix.ui.component.Component;
 import io.jmix.ui.component.GroupTable;
 import io.jmix.ui.component.Table;
+import io.jmix.ui.model.CollectionContainer;
+import io.jmix.ui.model.CollectionLoader;
+import io.jmix.ui.model.InstanceContainer;
 import io.jmix.ui.screen.*;
 import com.company.homeworkloans.entity.Loan;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,12 +17,40 @@ import java.time.LocalDate;
 @LookupComponent("loansTable")
 public class LoanApproval extends StandardLookup<Loan> {
 	@Autowired
-	private GroupTable<Loan> loansTable;
+	private CollectionLoader<Loan> clientOtherLoansDl;
+	@Autowired
+	private CollectionContainer<Loan> clientOtherLoansDc;
 
 	@Install(to = "loansTable.[client.age]", subject = "columnGenerator")
 	private Component loansTableClientAgeColumnGenerator(final Loan loan) {
 		String age = String.valueOf(LocalDate.now().minusYears(loan.getClient().getBirthDate().getYear()).getYear());
 		return new Table.PlainTextCell(age);
 	}
+
+	@Subscribe
+	public void onAfterShow(final AfterShowEvent event) {
+		clearClientOtherLoansTable();
+	}
+
+
+	@Subscribe(id = "loansDc", target = Target.DATA_CONTAINER)
+	public void onLoansDcItemChange(final InstanceContainer.ItemChangeEvent<Loan> event) {
+		var currentLoan = event.getItem();
+		if (currentLoan == null) {
+			clearClientOtherLoansTable();
+			return;
+		} else {
+			clientOtherLoansDl.setParameter("clientId", currentLoan.getClient().getId());
+			clientOtherLoansDl.setParameter("loanId", currentLoan.getId());
+		}
+
+		clientOtherLoansDl.load();
+	}
+
+
+	private void clearClientOtherLoansTable() {
+		clientOtherLoansDc.getMutableItems().clear();
+	}
+
 
 }
